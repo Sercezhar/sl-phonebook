7ca76bffec8f7ff6f9a5083efacf42bd6cc03e5c
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-16 20:34:54 +0300

    update Contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,5 +1,38 @@
 function Contacts() {
-  return <div className="text-red-400 font-bold">IT'S A CONTACTS PAGE!</div>;
+  const binding: string[] = [
+    'top-[10%]',
+    'top-[20%]',
+    'top-[30%]',
+    'top-[40%]',
+    'top-1/2',
+    'top-[60%]',
+    'top-[70%]',
+    'top-[80%]',
+    'top-[90%]',
+  ];
+
+  return (
+    <div className="relative flex mx-auto w-fit h-[540px]">
+      <div className="mr-1 p-6 pr-12 w-[490px] h-full bg-white border-solid border-x-[6px] border-y-8 border-sky-400 rounded overflow-hidden">
+        <h2 className="mb-4 font-semibold text-xl text-center">
+          Create a contact
+        </h2>
+      </div>
+
+      <div className="mr-1 p-6 pr-12 w-[490px] h-full bg-white border-solid border-x-[6px] border-y-8 border-sky-400 rounded overflow-hidden">
+        <h2 className="mb-4 font-semibold text-xl text-center">Contacts</h2>
+      </div>
+
+      {binding.map((top, index) => (
+        <span
+          key={index}
+          className={`absolute ${top} left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
+        >
+          <span className="block w-14 h-2 bg-slate-200 rounded shadow-[inset_0_0_8px_-6px_#2C3E50] before:content-[''] before:absolute before:top-1/2 before:left-[-12px] before:transform before:-translate-y-1/2 before:w-6 before:h-6 before:bg-slate-400 before:rounded-full before:-z-[1] before:shadow-[inset_5px_0_1px_0px_#ccc;] after:content-[''] after:absolute after:top-1/2 after:right-[-12px] after:transform after:-translate-y-1/2 after:w-6 after:h-6 after:bg-slate-400 after:rounded-full after:-z-[1] after:shadow-[inset_-5px_0_1px_0px_#ccc;]"></span>
+        </span>
+      ))}
+    </div>
+  );
 }
 
 export default Contacts;
