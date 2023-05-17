1683b4050609a69723b68bd9ceab624ee590be62
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-17 22:08:49 +0300

    update ContactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,9 @@
+import { patternPhone } from '@/constants/regExPatterns';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import Input from './form/Input';
+import Button from './ui/Button';
 
 interface ContactProps {
   firstName: string;
@@ -10,9 +12,6 @@ interface ContactProps {
 }
 
 function ContactForm() {
-  const phoneRegExp =
-    /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
-
   const contactSchema = yup.object().shape({
     firstName: yup.string().required('is a required field'),
     lastName: yup.string(),
@@ -20,7 +19,7 @@ function ContactForm() {
       .string()
       .required('is a required field')
       .min(5, 'must be at least 5 characters')
-      .matches(phoneRegExp, 'is not valid'),
+      .matches(patternPhone, 'is not valid'),
   });
 
   const {
@@ -65,12 +64,7 @@ function ContactForm() {
         />
       </div>
 
-      <button
-        className="mx-auto px-6 py-2 w-fit bg-sky-400 font-medium text-white rounded transition-opacity hover:opacity-80 outline-none focus:ring-4 focus:ring-sky-200"
-        type="submit"
-      >
-        Add contact
-      </button>
+      <Button type="submit" text="Add contact" />
     </form>
   );
 }
