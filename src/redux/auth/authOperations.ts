2a799655d36aaf0977ea4cc175185038d4e26c6f
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-19 23:33:27 +0300

    add authOperations

---
+++ src/redux/auth/authOperations.ts
@@ -0,0 +1,81 @@
+import { LogInAttributes, RegisterAttributes } from '@/types/authTypes';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { RootState } from '../store';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+function setAuthHeader(token: string) {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
+
+function clearAuthHeader() {
+  axios.defaults.headers.common.Authorization = '';
+}
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials: RegisterAttributes, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/signup', credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (credentials: LogInAttributes, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/login', credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+export const logOut = createAsyncThunk(
+  'auth/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      await axios.post('/users/logout');
+      clearAuthHeader();
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+export const refreshUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, { getState, rejectWithValue }) => {
+    const state = getState() as RootState;
+    const { token } = state.auth;
+
+    if (!token) {
+      return rejectWithValue('No token found');
+    }
+
+    setAuthHeader(token);
+
+    try {
+      const { data } = await axios.get('/users/current');
+      return data;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
