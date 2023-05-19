d25c5c30517c2be7ea1c3d1bd7bc5692ed9e982b
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-19 23:34:50 +0300

    add useAuth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,39 @@
+import { useAppDispatch, useAppSelector } from '@/redux/constants';
+import {
+  logIn,
+  logOut,
+  refreshUser,
+  register,
+} from '@/redux/auth/authOperations';
+import {
+  LogInAttributes,
+  RegisterAttributes,
+  UserAttributes,
+} from '@/types/authTypes';
+
+export function useAuth() {
+  const dispatch = useAppDispatch();
+
+  const user: UserAttributes = useAppSelector(state => state.auth.user);
+  const token: string | null = useAppSelector(state => state.auth.token);
+  const isLoggedIn: boolean = useAppSelector(state => state.auth.isLoggedIn);
+  const isRefreshing: boolean = useAppSelector(
+    state => state.auth.isRefreshing
+  );
+
+  const handleRegister = (data: RegisterAttributes) => dispatch(register(data));
+  const handleLogIn = (data: LogInAttributes) => dispatch(logIn(data));
+  const handleOut = () => dispatch(logOut());
+  const handleRefreshUser = () => dispatch(refreshUser());
+
+  return {
+    user,
+    token,
+    isLoggedIn,
+    isRefreshing,
+    registerUser: handleRegister,
+    logInUser: handleLogIn,
+    logOutUser: handleOut,
+    refreshUser: handleRefreshUser,
+  };
+}
