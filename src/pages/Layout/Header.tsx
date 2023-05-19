a1c6cc6f08fa0e0d61d5ec1ef717c5d25054ba43
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-19 23:36:28 +0300

    update Header

--- src/pages/Layout/Header.tsx
+++ src/pages/Layout/Header.tsx
@@ -1,16 +1,26 @@
+import UserMenu from '@/components/UserMenu';
+import { useAuth } from '@/hooks/useAuth';
 import AuthNav from '../../components/AuthNav';
 import Logo from '../../components/Logo';
 import Navigation from '../../components/Navigation';
 
 function Header() {
+  const { isLoggedIn } = useAuth();
+
   return (
-    <header className="sticky top-0 mb-8 bg-sky-400 drop-shadow z-20">
+    <header className="sticky top-0 mb-8 h-[56px] bg-sky-400 drop-shadow z-20">
       <div className="container mx-auto px-4 flex items-center justify-between">
-        <Navigation />
+        <div className="w-full max-w-[40%]">
+          <Navigation />
+        </div>
 
-        <Logo />
+        <div className="flex justify-center w-full max-w-[20%]">
+          <Logo />
+        </div>
 
-        <AuthNav />
+        <div className="flex justify-end w-full max-w-[40%]">
+          {isLoggedIn ? <UserMenu /> : <AuthNav />}
+        </div>
       </div>
     </header>
   );
