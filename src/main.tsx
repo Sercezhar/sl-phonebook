325f63cc50ec697929fba6b048520de68150a916
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-22 02:53:08 +0300

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
-import { store, persistor } from './redux/store';
-import { router } from './router/router';
 import { PersistGate } from 'redux-persist/integration/react';
+import './index.css';
+import { persistor, store } from './redux/store';
+import { router } from './router/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
