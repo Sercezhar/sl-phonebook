6ab02ec47d76e9b04c6bf9a911c18ac4866ed8f8
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-22 06:55:43 +0300

    update Filter

--- src/pages/Contacts/Filter.tsx
+++ src/pages/Contacts/Filter.tsx
@@ -1,4 +1,18 @@
+import { filterSelector } from '@/redux/filter/filterSelectors';
+import { setFilter } from '@/redux/filter/filterSlice';
+import { ChangeEvent } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 function Filter() {
+  const dispatch = useDispatch();
+
+  const filter = useSelector(filterSelector);
+
+  function handleFilterInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.currentTarget;
+    dispatch(setFilter(value));
+  }
+
   return (
     <div className="mb-4">
       <label
@@ -8,10 +22,12 @@ function Filter() {
         Search among contacts
       </label>
       <input
+        className="block p-2.5 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-2 focus:ring-sky-200"
         type="text"
         id="filter"
-        className="block p-2.5 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-2 focus:ring-sky-200"
         placeholder="John Doe"
+        onChange={handleFilterInputChange}
+        value={filter}
         required
       />
     </div>
