2352406f6d8ddd80258f8cdddcfa1ae8c1be405a
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-24 00:02:40 +0300

    update UserMenu

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -7,7 +7,7 @@ function UserMenu() {
   return (
     <div className="flex items-center gap-4 font-medium text-right text-white">
       <div>
-        <p className="text-sm">Greetings,</p>
+        <p className="text-sm">Greetings</p>
         <p className="max-w-[260px] text-ellipsis overflow-hidden">
           {user.name}
         </p>
