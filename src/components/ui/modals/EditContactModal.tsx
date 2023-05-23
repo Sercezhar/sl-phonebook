7db89e6176ef9b35b8926d2a13940f1381d701da
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-23 22:26:58 +0300

    add EditContactModal

---
+++ src/components/ui/modals/EditContactModal.tsx
@@ -0,0 +1,26 @@
+import EditContactForm from '@/pages/Contacts/EditContactForm';
+import { ContactAttributes } from '@/types';
+import Modal from './Modal';
+
+type ModalProps = {
+  isModalOpen: boolean;
+  contact: ContactAttributes | null;
+  onClose: () => void;
+};
+
+function EditContactModal({ isModalOpen, contact, onClose }: ModalProps) {
+  return (
+    <Modal
+      isModalOpen={isModalOpen}
+      isButtons={false}
+      title="Editing a contact"
+      onConfirmText="Edit contact"
+      onClose={onClose}
+      onConfirm={() => {}}
+    >
+      <EditContactForm contact={contact} onClose={onClose} />
+    </Modal>
+  );
+}
+
+export default EditContactModal;
