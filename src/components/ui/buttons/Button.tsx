d76e69cd2a1fbc7eac5a3ed9ce42e93330af5ac1
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-23 22:21:33 +0300

    update ui folder

---
+++ src/components/ui/buttons/Button.tsx
@@ -0,0 +1,21 @@
+import { ButtonType } from '@/types';
+
+interface ButtonProps {
+  type?: ButtonType;
+  text: string;
+  onClick?: () => void;
+}
+
+function Button({ type = 'button', text, onClick }: ButtonProps) {
+  return (
+    <button
+      className="flex mx-auto px-6 py-2 w-fit bg-sky-400 font-medium text-white border border-sky-400 rounded transition-opacity outline-none hover:opacity-80 focus:ring-4 focus:ring-sky-200"
+      type={type}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button;
