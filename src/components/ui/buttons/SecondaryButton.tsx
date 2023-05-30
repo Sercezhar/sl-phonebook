5000a2840c06512bcac9f36da89661291456c6d9
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-30 18:05:22 +0300

    update SecondaryButton

--- src/components/ui/buttons/SecondaryButton.tsx
+++ src/components/ui/buttons/SecondaryButton.tsx
@@ -1,9 +1,9 @@
-interface ButtonProps {
+interface SecondaryButtonProps {
   text: string;
   onClick?: () => void;
 }
 
-function SecondaryButton({ text, onClick }: ButtonProps) {
+function SecondaryButton({ text, onClick }: SecondaryButtonProps) {
   return (
     <button
       className="px-6 py-2 bg-white text-sky-400 border border-sky-400 rounded transition-colors outline-none hover:bg-sky-50 focus:ring-4 focus:ring-sky-200"
