871adbdcd6d79c5c2f082a16cfdf5adc5ea71f4a
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-30 17:56:01 +0300

    update MobileMenu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,25 +1,31 @@
 import { useAuth } from '@/hooks/useAuth';
+import useClickOutside from '@/hooks/useClickOutside';
 import classNames from 'classnames';
 import { useState } from 'react';
 import AuthNav from './AuthNav';
-import Navigation from './Navigation';
+import PrimaryNav from './PrimaryNav';
 import UserMenu from './UserMenu';
-import NavigationToggle from './ui/buttons/NavigationToggle';
+import CloseMenuButton from './ui/buttons/CloseMenuButton';
+import OpenMenuButton from './ui/buttons/OpenMenuButton';
 
 function MobileMenu() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const { isLoggedIn } = useAuth();
 
+  const ref = useClickOutside(() => handleMenuToggle(false));
+
+  function handleMenuToggle(isOpen: boolean) {
+    setIsMenuOpen(isOpen);
+    document.body.classList.toggle('overflow-hidden');
+  }
+
   return (
     <div className="flex lg:hidden">
-      <NavigationToggle
-        isMenuOpen={isMenuOpen}
-        setIsMenuOpen={() => setIsMenuOpen(prev => !prev)}
-      />
+      <OpenMenuButton setIsMenuOpen={() => handleMenuToggle(true)} />
 
       <div
         className={classNames(
-          'absolute top-full left-0 right-0 h-[calc(100dvh-56px)] bg-black transition overflow-hidden',
+          'absolute top-0 left-0 right-0 h-[100dvh] bg-black transition overflow-hidden',
 
           isMenuOpen
             ? 'bg-opacity-30 pointer-events-auto delay-0'
@@ -27,14 +33,17 @@ function MobileMenu() {
         )}
       >
         <div
+          ref={ref}
           className={classNames(
-            'absolute top-0 right-0 flex flex-col justify-between p-4 w-64 h-full font-medium bg-white transition-transform shadow overflow-hidden',
+            'absolute top-0 right-0 flex flex-col justify-between py-12 px-6 w-64 sm:w-96 h-full font-medium bg-white transition-transform shadow overflow-hidden',
 
             isMenuOpen ? 'transform-none delay-100' : 'translate-x-full delay-0'
           )}
         >
+          <CloseMenuButton setIsMenuOpen={() => handleMenuToggle(false)} />
+
           <div>
-            <Navigation closeMenu={() => setIsMenuOpen(false)} />
+            <PrimaryNav closeMenu={() => setIsMenuOpen(false)} />
 
             {!isLoggedIn && <AuthNav closeMenu={() => setIsMenuOpen(false)} />}
           </div>
