d24682e6399f9b5334c028b72d34c0d0bdcceb80
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-30 17:58:54 +0300

    add CloseMenuButton

---
+++ src/components/ui/buttons/CloseMenuButton.tsx
@@ -0,0 +1,19 @@
+import { HiX } from 'react-icons/hi';
+import { MenuButtonProps } from '../../../types/buttons';
+
+function CloseMenuButton({ setIsMenuOpen }: MenuButtonProps) {
+  return (
+    <button
+      className="group absolute top-2 right-4 rounded"
+      type="button"
+      onClick={setIsMenuOpen}
+    >
+      <HiX
+        size={40}
+        className="fill-gray-600 transition-colors group-hover:fill-sky-400"
+      />
+    </button>
+  );
+}
+
+export default CloseMenuButton;
