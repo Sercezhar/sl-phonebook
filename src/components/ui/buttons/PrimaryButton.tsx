d06d5dae05d1b67f88fccd5ad0f400928c92dfb1
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-30 17:58:22 +0300

    rename Button to PrimaryButton

--- src/components/ui/buttons/PrimaryButton.tsx
+++ src/components/ui/buttons/PrimaryButton.tsx
@@ -1,12 +1,12 @@
-import { ButtonType } from '@/types';
+import { ButtonType } from '../../../types/buttons';
 
-interface ButtonProps {
+interface PrimaryButtonProps {
   type?: ButtonType;
   text: string;
   onClick?: () => void;
 }
 
-function Button({ type = 'button', text, onClick }: ButtonProps) {
+function PrimaryButton({ type = 'button', text, onClick }: PrimaryButtonProps) {
   return (
     <button
       className="flex mx-auto px-6 py-2 w-fit bg-sky-400 font-medium text-white border border-sky-400 rounded transition-opacity outline-none hover:opacity-80 focus:ring-4 focus:ring-sky-200"
@@ -18,4 +18,4 @@ function Button({ type = 'button', text, onClick }: ButtonProps) {
   );
 }
 
-export default Button;
+export default PrimaryButton;
