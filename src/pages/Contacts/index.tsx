e7663d8413304a531e77af7b617786c2a1c86d37
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-30 18:06:38 +0300

    update Contacts

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -6,16 +6,18 @@ import ContactsList from './ContactsList';
 function Contacts() {
   return (
     <div className="relative flex mx-auto w-full lg:w-fit h-full lg:h-[540px]">
-      <div className="hidden lg:block mr-[2px] p-6 pr-12 w-[490px] h-full bg-white border-solid border-x-[6px] border-y-8 border-sky-400 rounded overflow-hidden">
-        <h2 className="mb-4 font-semibold text-xl text-center">
+      <div className="hidden lg:block mr-[4px] p-6 pr-12 w-[490px] h-full bg-white border-solid border-x-[6px] border-y-8 border-sky-400 rounded overflow-hidden">
+        <h2 className="mb-4 font-semibold text-xl text-center text-gray-700">
           Create a contact
         </h2>
 
         <ContactsForm />
       </div>
 
-      <div className="ml-[2px] p-1 lg:p-6 lg:pl-12 w-full lg:w-[490px] h-full bg-white border-solid lg:border-x-[6px] lg:border-y-8 border-sky-400 rounded overflow-hidden">
-        <h2 className="mb-4 font-semibold text-xl text-center">Contacts</h2>
+      <div className="sm:max-lg:mx-auto p-1 lg:p-6 lg:pl-12 w-full sm:w-[490px] h-full bg-white border-solid lg:border-x-[6px] lg:border-y-8 border-sky-400 rounded overflow-hidden">
+        <h2 className="mb-4 font-semibold text-xl text-center text-gray-700">
+          Contacts
+        </h2>
 
         <Filter />
 
