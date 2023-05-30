04873c8f86b41e71b2a2671ad13ce737100d66d1
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-30 17:54:41 +0300

    add NavigationLink

---
+++ src/components/NavigationLink.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames';
+import { ReactNode } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export interface NavigationLinkProps {
+  to: string;
+  closeMenu?: () => void;
+  children: ReactNode;
+}
+
+function NavigationLink({ to, closeMenu, children }: NavigationLinkProps) {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        classNames(
+          'relative flex items-center py-4 font-semibold lg:font-medium text-3xl lg:text-base after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-white',
+          isActive
+            ? 'after:block text-sky-400 lg:text-white'
+            : 'after:hidden text-gray-700 lg:text-white'
+        )
+      }
+      onClick={closeMenu}
+    >
+      {children}
+    </NavLink>
+  );
+}
+
+export default NavigationLink;
