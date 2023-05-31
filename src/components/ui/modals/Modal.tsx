78c1e9b60ced8181dd24e3e2f4018853cd29d38d
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-05-31 03:26:34 +0300

    update Modal

--- src/components/ui/modals/Modal.tsx
+++ src/components/ui/modals/Modal.tsx
@@ -1,4 +1,6 @@
-import useClickOutside from '@/hooks/useClickOutside';
+import { useClickOutside } from '@/hooks/useClickOutside';
+import { useKeyPress } from '@/hooks/useKeyPress';
+import { animated, useSpring, useTransition } from '@react-spring/web';
 import { ReactNode } from 'react';
 import PrimaryButton from '../buttons/PrimaryButton';
 import SecondaryButton from '../buttons/SecondaryButton';
@@ -22,40 +24,66 @@ function Modal({
   onConfirm,
   children,
 }: ModalProps) {
+  useKeyPress('Escape', () => onClose());
+
   const ref = useClickOutside(onClose);
 
-  return isModalOpen ? (
-    <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center bg-black/[.3] z-50">
-      <div
-        ref={ref}
-        className="flex flex-col gap-6 p-6 w-[288px] sm:w-[400px] font-medium bg-white rounded shadow-xl"
-      >
-        <h2 className="font-semibold text-xl">{title}</h2>
+  const modalTransition = useTransition(isModalOpen, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: {
+      duration: 150,
+    },
+  });
 
-        <div>
-          {children ? (
-            children
-          ) : (
-            <p className="text-gray-500">
-              The contact will be removed from the list permanently
-            </p>
-          )}
-        </div>
+  const contentTransition = useSpring({
+    opacity: isModalOpen ? 1 : 0,
+    scale: isModalOpen ? 1 : 0.5,
+    config: {
+      duration: 150,
+    },
+  });
 
-        {isButtons && (
-          <ul className="flex gap-4 justify-end">
-            <li>
-              <SecondaryButton text="Cancel" onClick={onClose} />
-            </li>
+  return modalTransition(
+    (styles, isModalOpen) =>
+      isModalOpen && (
+        <animated.div
+          style={styles}
+          className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center bg-black/[.3] z-50"
+        >
+          <animated.div
+            style={contentTransition}
+            ref={ref}
+            className="flex flex-col gap-6 p-6 w-[288px] sm:w-[400px] font-medium bg-white rounded shadow-xl"
+          >
+            <h2 className="font-semibold text-xl">{title}</h2>
 
-            <li>
-              <PrimaryButton text={onConfirmText} onClick={onConfirm} />
-            </li>
-          </ul>
-        )}
-      </div>
-    </div>
-  ) : null;
+            <div>
+              {children ? (
+                children
+              ) : (
+                <p className="text-gray-500">
+                  The contact will be removed from the list permanently
+                </p>
+              )}
+            </div>
+
+            {isButtons && (
+              <ul className="flex gap-4 justify-end">
+                <li>
+                  <SecondaryButton text="Cancel" onClick={onClose} />
+                </li>
+
+                <li>
+                  <PrimaryButton text={onConfirmText} onClick={onConfirm} />
+                </li>
+              </ul>
+            )}
+          </animated.div>
+        </animated.div>
+      )
+  );
 }
 
 export default Modal;
