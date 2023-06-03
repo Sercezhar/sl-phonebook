f58378a3b7efa35dea649c21daa0eb3900bd7f28
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-03 04:15:18 +0300

    update Loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,34 @@
-function Loader() {
+type Position = 'static' | 'absolute';
+
+interface LoaderProps {
+  position?: Position;
+  background?: string;
+  size?: string;
+  borderWidth?: string;
+}
+
+function Loader({
+  position = 'absolute',
+  background = 'rgb(255 255 255 / .6)',
+  size = '6rem',
+  borderWidth = '10px',
+}: LoaderProps) {
   return (
     <div
       role="status"
-      className="absolute inset-x-0 inset-y-0 z-10 flex items-center justify-center bg-white/[.6]"
+      className="absolute inset-x-0 inset-y-0 z-10 flex items-center justify-center"
+      style={{
+        position,
+        background,
+      }}
     >
       <div
-        className="inline-block h-24 w-24 animate-spin rounded-full border-[9px] border-current border-t-sky-400/[.2] text-sky-400"
+        className="inline-block animate-spin rounded-full border-current border-t-sky-400/[.2] text-sky-400"
+        style={{
+          width: size,
+          height: size,
+          borderWidth,
+        }}
         role="status"
         aria-label="loading"
       >
