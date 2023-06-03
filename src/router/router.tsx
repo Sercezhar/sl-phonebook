874152f0f454ddfaa0ad26559a8f021ea13924f0
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-04 02:37:31 +0300

    update router.tsx

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import PrivateRoute from '@/components/PrivateRoute';
 import RestrictedRoute from '@/components/RestrictedRoute';
+import ErrorPage from '@/pages/ErrorPage';
 import Layout from '@/pages/Layout';
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
@@ -8,7 +9,6 @@ const Homepage = lazy(() => import('@/pages/Homepage'));
 const Contacts = lazy(() => import('@/pages/Contacts'));
 const LogIn = lazy(() => import('@/pages/LogIn'));
 const SignUp = lazy(() => import('@/pages/SignUp'));
-const ErrorPage = lazy(() => import('@/pages/ErrorPage'));
 
 export const router = createBrowserRouter(
   [
