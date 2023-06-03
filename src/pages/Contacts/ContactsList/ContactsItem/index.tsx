e6143f950e4ffd4d8cb2aa0b2f0135d4a2117b05
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-03 04:16:28 +0300

    update ContactsItem

--- src/pages/Contacts/ContactsList/ContactsItem/index.tsx
+++ src/pages/Contacts/ContactsList/ContactsItem/index.tsx
@@ -1,3 +1,5 @@
+import Loader from '@/components/Loader';
+import { useContactsStatus } from '@/hooks/useContactsStatus';
 import ContactsItemActions from './ContactsItemActions';
 
 interface ContactItemProps {
@@ -17,6 +19,8 @@ function ContactsItem({
   toggleMenu,
   setIsModalOpen,
 }: ContactItemProps) {
+  const { isDeleting } = useContactsStatus();
+
   return (
     <li className="grid grid-cols-[50px_auto_40px] border-b py-2 text-sm font-medium last:border-b-0">
       <span className="pointer-events-none flex h-[40px] w-[40px] select-none items-center justify-center rounded-full border-2 border-sky-400 bg-sky-50 text-xl capitalize text-sky-400">
@@ -28,13 +32,22 @@ function ContactsItem({
         <p className="overflow-hidden text-ellipsis">{number}</p>
       </div>
 
-      <div className="flex items-center justify-end">
-        <ContactsItemActions
-          id={id}
-          isMenuVisible={isMenuVisible}
-          toggleMenu={toggleMenu}
-          setIsModalOpen={setIsModalOpen}
-        />
+      <div className="flex items-center justify-center">
+        {isDeleting && isMenuVisible ? (
+          <Loader
+            position="static"
+            background="none"
+            size="1.5rem"
+            borderWidth="4px"
+          />
+        ) : (
+          <ContactsItemActions
+            id={id}
+            isMenuVisible={isMenuVisible}
+            toggleMenu={toggleMenu}
+            setIsModalOpen={setIsModalOpen}
+          />
+        )}
       </div>
     </li>
   );
