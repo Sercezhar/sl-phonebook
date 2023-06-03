6a3bd86f85c2a32d919be83736fb276e601a783f
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-04 02:38:37 +0300

    update authSlice

--- src/redux/auth/authSlice.ts
+++ src/redux/auth/authSlice.ts
@@ -1,17 +1,18 @@
-import { UserAttributes } from '@/types/user';
+import { authState } from '@/types/redux';
 import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import {
+  handleFulfilledLogIn,
+  handleFulfilledLogOut,
+  handleFulfilledRefresh,
+  handleFulfilledRegister,
+  handlePending,
+  handlePendingRefresh,
+  handleRejected,
+  handleRejectedRefresh,
+} from './authHandlers';
 import { logIn, logOut, refreshUser, register } from './authOperations';
-
-interface authState {
-  user: UserAttributes;
-  token: string | null;
-  isLoading: boolean;
-  isLoggedIn: boolean;
-  isRefreshing: boolean;
-  error: string | null;
-}
 
 const initialState: authState = {
   user: { name: null, email: null },
@@ -22,58 +23,18 @@ const initialState: authState = {
   error: null,
 };
 
-function handlePending(state: authState) {
-  state.isLoading = true;
-}
-
-function handleFulfilled(state: authState) {
-  state.isLoading = false;
-  state.error = null;
-}
-
-function handleRejected(
-  state: authState,
-  { payload }: { payload: string | undefined }
-) {
-  state.isLoading = false;
-  state.error = payload || 'Something went wrong';
-}
-
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
   extraReducers: builder =>
     builder
-      .addCase(register.fulfilled, (state, { payload }) => {
-        handleFulfilled(state);
-        state.user = payload.user;
-        state.token = payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(logIn.fulfilled, (state, { payload }) => {
-        handleFulfilled(state);
-        state.user = payload.user;
-        state.token = payload.token;
-        state.isLoggedIn = true;
-      })
-      .addCase(logOut.fulfilled, state => {
-        handleFulfilled(state);
-        state.user = { name: null, email: null };
-        state.token = null;
-        state.isLoggedIn = false;
-      })
-      .addCase(refreshUser.pending, state => {
-        state.isRefreshing = true;
-      })
-      .addCase(refreshUser.fulfilled, (state, { payload }) => {
-        state.user = payload;
-        state.isLoggedIn = true;
-        state.isRefreshing = false;
-      })
-      .addCase(refreshUser.rejected, state => {
-        state.isRefreshing = false;
-      })
+      .addCase(register.fulfilled, handleFulfilledRegister)
+      .addCase(logIn.fulfilled, handleFulfilledLogIn)
+      .addCase(logOut.fulfilled, handleFulfilledLogOut)
+      .addCase(refreshUser.pending, handlePendingRefresh)
+      .addCase(refreshUser.fulfilled, handleFulfilledRefresh)
+      .addCase(refreshUser.rejected, handleRejectedRefresh)
       .addMatcher(
         isAnyOf(register.pending, logIn.pending, logOut.pending),
         handlePending
