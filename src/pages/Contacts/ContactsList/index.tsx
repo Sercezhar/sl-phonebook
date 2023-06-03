0c2e2bf4095e855c2d7aee3453caec1ef5670a3a
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-03 04:16:54 +0300

    update ContactsList

--- src/pages/Contacts/ContactsList/index.tsx
+++ src/pages/Contacts/ContactsList/index.tsx
@@ -2,7 +2,13 @@ import CreateContactButton from '@/components/ui/buttons/CreateContactButton';
 import CreateContactModal from '@/components/ui/modals/CreateContactModal';
 import EditContactModal from '@/components/ui/modals/EditContactModal';
 import Modal from '@/components/ui/modals/Modal';
-import { useContacts } from '@/hooks/useContacts';
+import { useContactsStatus } from '@/hooks/useContactsStatus';
+import {
+  deleteContact,
+  getContacts,
+} from '@/redux/contacts/contactsOperations';
+import { contactsSelector } from '@/redux/contacts/contactsSelectors';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { ContactAttributes } from '@/types/contact';
 import { useEffect, useMemo, useState } from 'react';
 import ContactsItem from './ContactsItem';
@@ -18,10 +24,13 @@ function ContactsList({ filter }: ContactsListProps) {
     useState<ContactAttributes | null>(null);
   const [isModalOpen, setIsModalOpen] = useState<string | null>(null);
 
-  const { contacts, isLoading, getContacts, deleteContact } = useContacts();
+  const dispatch = useAppDispatch();
+
+  const contacts: ContactAttributes[] = useAppSelector(contactsSelector);
+  const { isFetching, isUpdating } = useContactsStatus();
 
   useEffect(() => {
-    getContacts();
+    dispatch(getContacts());
   }, []);
 
   const filteredContacts = useMemo(() => {
@@ -41,7 +50,7 @@ function ContactsList({ filter }: ContactsListProps) {
       return;
     }
 
-    deleteContact(id);
+    dispatch(deleteContact(id));
     setIsModalOpen(null);
   }
 
@@ -50,13 +59,22 @@ function ContactsList({ filter }: ContactsListProps) {
     setIsModalOpen(null);
   }
 
-  if (isLoading) {
-    return <ContactsItemSkeleton />;
+  if (isFetching) {
+    const skeletons = [...Array(6).keys()];
+
+    return (
+      <ul>
+        {skeletons.map(i => (
+          <ContactsItemSkeleton key={i} />
+        ))}
+      </ul>
+    );
   }
 
   if (contacts.length === 0) {
     return <Notification message="The contact list is empty" />;
   }
+
   if (filteredContacts.length === 0) {
     return <Notification message="No contacts found" />;
   }
@@ -64,17 +82,21 @@ function ContactsList({ filter }: ContactsListProps) {
   return (
     <div>
       <ul className="h-full lg:h-[350px] lg:overflow-y-auto">
-        {filteredContacts.map(({ id, name, number }) => (
-          <ContactsItem
-            key={id}
-            id={id}
-            name={name}
-            number={number}
-            isMenuVisible={clickedContact?.id === id}
-            toggleMenu={() => handleSetClickedContact({ id, name, number })}
-            setIsModalOpen={setIsModalOpen}
-          />
-        ))}
+        {filteredContacts.map(({ id, name, number }) =>
+          isUpdating && clickedContact?.id === id ? (
+            <ContactsItemSkeleton key={id} />
+          ) : (
+            <ContactsItem
+              key={id}
+              id={id}
+              name={name}
+              number={number}
+              isMenuVisible={clickedContact?.id === id}
+              toggleMenu={() => handleSetClickedContact({ id, name, number })}
+              setIsModalOpen={setIsModalOpen}
+            />
+          )
+        )}
       </ul>
 
       <Modal
@@ -86,6 +108,8 @@ function ContactsList({ filter }: ContactsListProps) {
         isModalOpen={isModalOpen === 'edit'}
         contact={clickedContact}
         onClose={() => handleModalOnClose()}
+        closeModal={() => setIsModalOpen(null)}
+        closeActions={() => setClickedContact(null)}
       />
       <CreateContactModal
         isModalOpen={isModalOpen === 'create'}
