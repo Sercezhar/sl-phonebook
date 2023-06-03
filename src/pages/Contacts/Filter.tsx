f0f040e6b6b05b67e39b00e5894c9a787c626e1e
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-03 07:33:52 +0300

    update Filter

--- src/pages/Contacts/Filter.tsx
+++ src/pages/Contacts/Filter.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent } from 'react';
+import { HiSearch, HiX } from 'react-icons/hi';
 
 interface FilterProps {
   filter: string;
@@ -19,15 +20,33 @@ function Filter({ filter, setFilter }: FilterProps) {
       >
         Search among contacts
       </label>
-      <input
-        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 outline-none focus:ring-2 focus:ring-sky-200"
-        type="text"
-        id="filter"
-        placeholder="John Doe"
-        onChange={handleFilterInputChange}
-        value={filter}
-        required
-      />
+
+      <div className="relative">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <HiSearch size={20} className="fill-gray-400" />
+        </div>
+
+        <input
+          className="peer block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 outline-none focus:ring-2 focus:ring-sky-200"
+          type="text"
+          id="filter"
+          placeholder="John Doe"
+          onChange={handleFilterInputChange}
+          value={filter}
+          required
+        />
+
+        <button
+          className="group absolute inset-y-0 right-0 block px-3 peer-placeholder-shown:hidden"
+          type="button"
+          onClick={() => setFilter('')}
+        >
+          <HiX
+            size={18}
+            className=" fill-gray-500 transition-colors group-hover:fill-sky-400"
+          />
+        </button>
+      </div>
     </div>
   );
 }
