acbf9d4b11e0869cd360cdcd1e49f55ef886970d
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-03 04:16:44 +0300

    update ContactsItemSkeleton

--- src/pages/Contacts/ContactsList/ContactsItemSkeleton.tsx
+++ src/pages/Contacts/ContactsList/ContactsItemSkeleton.tsx
@@ -1,22 +1,13 @@
 function ContactsItemSkeleton() {
-  const skeletons = [...Array(6).keys()];
-
   return (
-    <ul className="h-[350px] overflow-y-auto">
-      {skeletons.map(index => (
-        <li
-          key={index}
-          className="flex animate-pulse border-b py-2 last:border-b-0"
-        >
-          <div className="mr-2 h-[40px] w-[40px] rounded-full bg-gray-200"></div>
+    <li className="flex animate-pulse border-b py-2 last:border-b-0">
+      <div className="mr-2 h-[40px] w-[40px] rounded-full bg-gray-200"></div>
 
-          <div className="flex flex-col justify-center gap-1">
-            <div className="h-[18px] w-40 rounded bg-gray-200 "></div>
-            <div className="h-[18px] w-52 rounded bg-gray-200"></div>
-          </div>
-        </li>
-      ))}
-    </ul>
+      <div className="flex flex-col justify-center gap-1">
+        <div className="h-[18px] w-40 rounded bg-gray-200 "></div>
+        <div className="h-[18px] w-52 rounded bg-gray-200"></div>
+      </div>
+    </li>
   );
 }
 
