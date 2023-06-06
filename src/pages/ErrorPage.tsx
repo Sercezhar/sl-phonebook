ca71bdd1d225c074e12a68545d8280cd54cbcab1
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-06 16:09:17 +0300

    update ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,17 +1,23 @@
-import { Link } from 'react-router-dom';
+import NavigationButton from '@/components/ui/buttons/NavigationButton';
 
 function ErrorPage() {
   return (
-    <div className="flex h-[90dvh] flex-col items-center justify-center font-medium text-gray-600">
-      <h2 className="text-9xl">404</h2>
-      <p className="mb-6 text-2xl uppercase">Page not found</p>
+    <div className="flex h-[100dvh] items-center justify-center py-7 text-gray-700 max-md:flex-col-reverse">
+      <div className="w-[300px] md:mr-8 lg:mr-10 lg:w-[400px]">
+        <h2 className="mb-6 text-2xl font-bold">
+          Oops! It appears you've veered off course.
+        </h2>
 
-      <Link
-        to="/"
-        className="rounded bg-sky-400 px-6 py-2 font-medium text-white transition-opacity hover:opacity-80"
-      >
-        Home
-      </Link>
+        <div className="mb-8">
+          Just look for the trusty Home button, patiently awaiting your return.
+          Give it a gentle press, and it will whisk you away to familiar
+          territory.
+        </div>
+
+        <NavigationButton to="/" text="Home" />
+      </div>
+
+      <div className="h-[300px] w-[300px] bg-error-404 bg-contain bg-center bg-no-repeat max-md:mb-6 md:h-[400px] md:w-[400px]"></div>
     </div>
   );
 }
