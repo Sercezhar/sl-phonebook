0db97efc7b2a93c84bc71ce3414acd9ef154ac33
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-06 16:09:08 +0300

    update Homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,24 @@
+import NavigationButton from '@/components/ui/buttons/NavigationButton';
+
 function Homepage() {
   return (
-    <div className="pointer-events-none flex h-[calc(100dvh-176px)] select-none items-center justify-center text-4xl font-bold uppercase text-sky-400 sm:text-6xl">
-      <h1 className="max-w-screen-sm text-center">
-        Your contacts in one place
-      </h1>
+    <div className="flex h-[calc(100dvh-112px)] items-center justify-center text-gray-700 max-md:flex-col-reverse">
+      <div className="flex w-[300px] flex-col md:w-[400px] lg:w-[500px]">
+        <div className="mb-6 w-full md:mb-8 md:w-[300px] lg:w-[400px]">
+          <h1 className="mb-6 text-2xl font-bold lg:text-3xl xl:text-4xl">
+            Keep your contacts in one place
+          </h1>
+
+          <p className="lg:text-xl">
+            Take control of your contacts with our intuitive and reliable
+            platform and stay connected effortlessly.
+          </p>
+        </div>
+
+        <NavigationButton text="Open Phonebook" />
+      </div>
+
+      <div className="h-[300px] w-[300px] bg-hero bg-contain bg-center bg-no-repeat max-md:mb-6 md:block md:h-full md:w-[400px] lg:w-[500px] xl:w-[600px]"></div>
     </div>
   );
 }
