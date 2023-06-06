19182d06e083011829fe8a43333e9c0e7bb4f1b0
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-06 16:09:00 +0300

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         montserrat: ['Montserrat', 'sans-serif'],
         caveat: ['Caveat', 'cursive'],
       },
+      backgroundImage: {
+        hero: "url('/illustrations/connection.svg')",
+        'error-404': "url('/illustrations/website-down.svg')",
+      },
     },
   },
   plugins: [],
