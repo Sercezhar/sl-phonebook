47cc0682677664ab3b07688e7810eb944ca0b098
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-07 15:10:48 +0300

    update Notification

--- src/pages/Contacts/ContactsList/Notification.tsx
+++ src/pages/Contacts/ContactsList/Notification.tsx
@@ -1,11 +1,20 @@
 interface NotificationProps {
   message: string;
+  image?: null | string;
 }
 
-function Notification({ message }: NotificationProps) {
+function Notification({ message, image }: NotificationProps) {
   return (
-    <div className="p-4 text-center text-lg font-medium text-gray-400">
-      {message}
+    <div>
+      <h3 className="mb-4 p-4 text-center text-lg font-medium text-gray-400">
+        {message}
+      </h3>
+
+      {image && (
+        <div
+          className={`mx-auto h-[140px] w-[200px] bg-contain bg-center bg-no-repeat ${image}`}
+        ></div>
+      )}
     </div>
   );
 }
