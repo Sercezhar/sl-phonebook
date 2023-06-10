91a4184b79d63261f328898fd200813172659fa7
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-11 00:19:04 +0300

    update Contacts

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -4,12 +4,14 @@ import RightSide from './RightSide';
 
 function Contacts() {
   return (
-    <div className="relative mx-auto mb-7 flex h-full w-full lg:h-[540px] lg:w-fit">
-      <LeftSide />
+    <div className="mb-10 lg:flex lg:h-[calc(100vh-176px)] lg:min-h-[540px] lg:items-center lg:justify-center">
+      <div className="relative flex h-full w-full lg:h-[540px] lg:w-fit 2xl:h-[710px]">
+        <LeftSide />
 
-      <Binding />
+        <Binding />
 
-      <RightSide />
+        <RightSide />
+      </div>
     </div>
   );
 }
