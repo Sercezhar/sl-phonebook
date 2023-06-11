f5ba5dc76cd63d2ad9dd230ff8b494f5e0934a30
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-06-11 05:37:04 +0300

    update styles

--- src/pages/Layout/Header/NavigationLink.tsx
+++ src/pages/Layout/Header/NavigationLink.tsx
@@ -14,7 +14,7 @@ function NavigationLink({ to, closeMenu, children }: NavigationLinkProps) {
       to={to}
       className={({ isActive }) =>
         classNames(
-          'relative flex items-center py-4 text-3xl font-semibold transition-colors before:absolute before:left-1/2 before:top-1/2 before:h-[60%] before:w-[130%] before:-translate-x-1/2 before:-translate-y-1/2 before:rounded before:bg-white before:opacity-0 before:transition-opacity before:content-[""] after:absolute after:bottom-0 after:left-0 after:h-1 after:w-full after:bg-white after:content-[""] hover:text-sky-400 hover:before:opacity-100 lg:text-base lg:font-medium',
+          'relative flex py-4 text-3xl font-semibold transition-colors lg:text-base lg:font-medium lg:before:absolute lg:before:left-1/2 lg:before:top-1/2 lg:before:h-[60%] lg:before:w-[130%] lg:before:-translate-x-1/2 lg:before:-translate-y-1/2 lg:before:rounded lg:before:bg-white lg:before:opacity-0 lg:before:transition-opacity lg:before:content-[""] lg:after:absolute lg:after:bottom-0 lg:after:left-0 lg:after:h-1 lg:after:w-full lg:after:bg-white lg:after:content-[""] lg:hover:text-sky-400 lg:hover:before:opacity-100',
           isActive
             ? 'text-sky-400 after:block max-lg:fill-sky-400 lg:text-white'
             : 'text-gray-700 after:hidden max-lg:fill-gray-500 lg:text-white'
@@ -22,7 +22,7 @@ function NavigationLink({ to, closeMenu, children }: NavigationLinkProps) {
       }
       onClick={closeMenu}
     >
-      <span className="z-10">{children}</span>
+      <span className="z-10 max-lg:flex max-lg:items-center">{children}</span>
     </NavLink>
   );
 }
